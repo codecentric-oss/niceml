f9014ce5443484d4fd73656d5c6299140b41d3c6
Nils Uhrberg <nils.uhrberg@codecentric.de>
2023-07-07 13:36:35 +0200

    fix: Adjust `ZippedCsvToParqProcessor` to new `list_dir` function`

--- niceml/utilities/ioutils.py
+++ niceml/utilities/ioutils.py
@@ -37,7 +37,7 @@ def list_dir(
         relpath(cur_file, path) for cur_file in list(cur_fs.listdir(path, detail=False))
     ]
     if filter_ext is not None:
-        files = [cur_file for cur_file in files if splitext(cur_file)[0] in filter_ext]
+        files = [cur_file for cur_file in files if splitext(cur_file)[1] in filter_ext]
     if recursive:
         folders = [
             cur_folder for cur_folder in files if cur_fs.isdir(join(path, cur_folder))
