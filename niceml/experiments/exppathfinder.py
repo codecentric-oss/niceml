2d310411d94aae32e8410b4b0299fc4b06620a4f
Nils <nils.uhrberg@codecentric.de>
2023-09-14 16:22:40 +0200

    fix: Return the experiment path instead of the `ExperimentInfo` if `exp_id` is not latest  (#76)
    ## 📥 Pull Request Description
    
    There was a bug that the `get_exp_filepath` function returns
    an`ExperimentInfo` instead of the experiment file path if the exp_id is
    something else than "latest". This is fixed with this pull request.
    
    ## 👀 Affected Areas
    
    - eval pipeline --> load experiment 
    
    ## 📝 Checklist
    
    Please make sure you've completed the following tasks before submitting
    this pull request:
    
    - [x] Pre-commit hooks were executed
    - [x] Changes have been reviewed by at least one other developer
    - [x] Tests have been added or updated to cover the changes (only
    necessary if the changes affect the executable code)
    - [x] All tests ran successfully
    - [x] All merge conflicts are resolved
    - [ ] Documentation has been updated to reflect the changes
    - [ ] Any necessary migrations have been run

--- niceml/experiments/exppathfinder.py
+++ niceml/experiments/exppathfinder.py
@@ -2,7 +2,10 @@
 from typing import List
 
 from niceml.data.storages.fsspecstorage import FSSpecStorage
-from niceml.experiments.experimenterrors import ExperimentNotFoundError
+from niceml.experiments.experimenterrors import (
+    ExperimentNotFoundError,
+    MultipleExperimentsFoundError,
+)
 from niceml.experiments.experimentinfo import ExperimentInfo
 from niceml.utilities.fsspec.locationutils import LocationConfig
 
@@ -21,7 +24,7 @@ def get_exp_filepath(fs_path_config: LocationConfig, exp_id: str):
             f"Experiment with id: {exp_id} not found in path: {fs_path_config.uri}"
         )
     if len(exps_w_id) > 1:
-        raise ExperimentNotFoundError(
+        raise MultipleExperimentsFoundError(
             f"Multiple experiments with id: {exp_id} found in path: {fs_path_config.uri}"
         )
-    return exps_w_id[0]
+    return exps_w_id[0].exp_filepath
