a3d494f35c9872bb5272a00cc94d264a44761da7
Denis Stalz-John <denis.stalz-john@codecentric.de>
2023-09-18 16:37:37 +0200

    doc: added two docstrings

--- niceml/dlframeworks/tensorflow/predictionfunctions/keraspredictionfunction.py
+++ niceml/dlframeworks/tensorflow/predictionfunctions/keraspredictionfunction.py
@@ -8,5 +8,6 @@ class KerasPredictionFunction(PredictionFunction):
     """Prediction function for keras models"""
 
     def predict(self, model, data_x) -> Any:
+        """uses a keras model to predict the data"""
         pred = model.predict_step(data_x).numpy()
         return pred
--- niceml/mlcomponents/predictionfunction/predictionfunction.py
+++ niceml/mlcomponents/predictionfunction/predictionfunction.py
@@ -4,6 +4,8 @@ from typing import Any
 
 
 class PredictionFunction(ABC):
+    """Abstract class for prediction functions"""
+
     @abstractmethod
     def predict(self, model, data_x) -> Any:
         """Predicts the given data with the given model"""
