9cebe9d8d688a21aec2f83ccd69cd23edab89cb5
Nils Uhrberg <nils.uhrberg@codecentric.de>
2024-09-12 20:08:43 +0200

    feat: Add the ability to add a niceML formatted timestamp based on a given datetime object
    The timestmap format used in niceml has a special foramt. If it is necessary to create this, it can now be created based on a given datetime object.

--- niceml/utilities/timeutils.py
+++ niceml/utilities/timeutils.py
@@ -1,14 +1,19 @@
 """Module for time related utilities"""
+
 from datetime import datetime
+from typing import Optional
 
 
-def generate_timestamp() -> str:
+def generate_timestamp(current_ts: Optional[datetime] = None) -> str:
     """Generates the timestamp to be used by versioning
+
+    Args:
+        current_ts: Optional datetime object that can be used to create a formatted timestamp.
 
     Returns:
         String representation of the current timestamp
     """
-    current_ts = datetime.now()
+    current_ts = current_ts or datetime.now()
     fmt = (
         "{d.year:04d}-{d.month:02d}-{d.day:02d}T{d.hour:02d}"
         ".{d.minute:02d}.{d.second:02d}.{ms:03d}Z"
